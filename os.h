33c1e33d5ae8af1ebe45634558b1dad8e3b6b012
Maxim Logaev <maximlogaev2001ezro@gmail.com>
2022-07-25 16:44:47 +0300

    Added windows detection

--- os.h
+++ os.h
@@ -4,6 +4,14 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+#if defined(_unix) || defined(__unix) || defined(__unix__) || defined(__CYGWIN__)
+#define OS_UNIX
+#elif defined(_WIN32) || defined(WIN32) || defined(WIN64) || defined(_WIN64) || defined(__MINGW32__) || defined(__MINGW64__)
+#define OS_WINDOWS
+#else
+#error "The platform is not supported. Implement functions for your platform in os.c"
+#endif
+
 #define OS_PATH_MAX 4096
 
 size_t os_get_fsize(const char *fname);
